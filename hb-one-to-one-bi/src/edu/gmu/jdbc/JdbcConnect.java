a8d6a4600d69898dcf3ce2913f30c160c9dd82df
Changhoon Ji <cji2@gmu.edu>
2020-01-18 07:43:02 -0500

    Not changed jdbcUrl (same db schema name), only Java code changed.

---
+++ hb-one-to-one-bi/src/edu/gmu/jdbc/JdbcConnect.java
@@ -0,0 +1,26 @@
+package edu.gmu.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JdbcConnect {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = 
+			"jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
+		String user = "gmustudent";
+		String pass = "gmustudent";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection successful!!!");
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+}
