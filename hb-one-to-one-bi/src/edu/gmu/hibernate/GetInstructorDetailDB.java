7f89f3437c02d81abf9c4cd939d40e409e93b599
Changhoon Ji <cji2@gmu.edu>
2020-01-18 11:10:09 -0500

    create main app. to retrieve instructor object using instructor detail.

---
+++ hb-one-to-one-bi/src/edu/gmu/hibernate/GetInstructorDetailDB.java
@@ -0,0 +1,46 @@
+package edu.gmu.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import edu.gmu.hibernate.entity.Instructor;
+import edu.gmu.hibernate.entity.InstructorDetail;
+
+public class GetInstructorDetailDB {
+
+	public static void main(String[] args) {
+
+		// create session factory.
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// start transaction.
+			session.beginTransaction();
+			
+			// get an instructor detail object by primary key / id
+			int id = 1;
+			InstructorDetail anInstructorDetail = session.get(InstructorDetail.class, id);
+			
+			// print detail object.
+			System.out.println("an Instructor Detail: " + anInstructorDetail);
+			
+			// print the associated instructor.
+			System.out.println("the associated instructor: " + anInstructorDetail.getInstructor());
+		
+			// commit the transaction.
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		} 
+		finally {
+			factory.close();
+		}
+	}
+}
